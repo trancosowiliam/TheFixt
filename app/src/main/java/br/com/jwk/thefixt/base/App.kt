061794da1148ec407626114a3f86e064d7ddc38a
trancosowilliam <wileleh@gmail.com>
2018-10-21 14:30:14 -0300

    injetando services

--- app/src/main/java/br/com/jwk/thefixt/base/App.kt
+++ app/src/main/java/br/com/jwk/thefixt/base/App.kt
@@ -1,10 +1,14 @@
 package br.com.jwk.thefixt.base
 
 import android.app.Application
+import br.com.jwk.thefixt.data.remote.ParseService
 import br.com.jwk.thefixt.module.*
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
+
+    private val parseService by inject<ParseService>()
 
     private val modules = listOf(
             applicationModule,
@@ -15,5 +19,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, modules)
+
+        parseService.init()
     }
 }
--- app/src/main/java/br/com/jwk/thefixt/module/ServiceModule.kt
+++ app/src/main/java/br/com/jwk/thefixt/module/ServiceModule.kt
@@ -1,9 +1,11 @@
 package br.com.jwk.thefixt.module
 
-import br.com.jwk.thefixt.data.remote.OMDbService
-import br.com.jwk.thefixt.data.remote.OMDbServiceImpl
+import br.com.jwk.thefixt.data.remote.*
 import org.koin.dsl.module.applicationContext
 
 val serviceModule = applicationContext {
+    factory { ParseServiceImpl(context = get()) as ParseService }
     factory { OMDbServiceImpl(retrofit = get()) as OMDbService }
+    factory { OwnerServiceImpl() as OwnerService }
+
 }
